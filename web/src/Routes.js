69dd6389e120cab54e7a62049fdd38ab6b42477a
Oğuzhan Atalay <oguzhnatly@gmail.com>
2021-03-29 10:43:56 +0300

    Login page ui improvements with react form elements.

--- web/src/Routes.js
+++ web/src/Routes.js
@@ -7,13 +7,16 @@
 // 'src/pages/HomePage/HomePage.js'         -> HomePage
 // 'src/pages/Admin/BooksPage/BooksPage.js' -> AdminBooksPage
 
-import { Router, Route } from '@redwoodjs/router'
+import { Router, Route, Redirect, routes } from '@redwoodjs/router'
 import NotFoundPage from 'src/pages/NotFoundPage/NotFoundPage.tsx'
 import LoginPage from 'src/pages/LoginPage/LoginPage.tsx'
+
+const HomePageRedirect = () => <Redirect to={routes.login()} />
 
 const Routes = () => {
   return (
     <Router>
+      <Route path="/" page={HomePageRedirect} name="home" />
       <Route path="/login" page={LoginPage} name="login" />
       <Route notfound page={NotFoundPage} />
     </Router>
--- web/src/pages/LoginPage/LoginPage.tsx
+++ web/src/pages/LoginPage/LoginPage.tsx
@@ -1,5 +1,12 @@
 import React from 'react'
-// import { Link, routes } from '@redwoodjs/router'
+import {
+  Form,
+  Label,
+  TextField,
+  PasswordField,
+  FieldError,
+  Submit
+} from '@redwoodjs/forms'
 
 const LoginPage = () => {
   return (
@@ -15,63 +22,63 @@ const LoginPage = () => {
             Sign in
           </h2>
         </div>
-        <form className="mt-8 space-y-6" action="#" method="POST">
-          <input type="hidden" name="remember" value="true" />
-          <div className="rounded-md shadow-sm -space-y-px">
-            <div>
-              <label htmlFor="email-address" className="sr-only">
-                Email address
-              </label>
-              <input
-                id="email-address"
-                name="email"
-                type="email"
-                autoComplete="email"
-                required
-                className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
-                placeholder="Email address"
-              />
-            </div>
-            <div>
-              <label htmlFor="password" className="sr-only">
-                Password
-              </label>
-              <input
-                id="password"
-                name="password"
-                type="password"
-                autoComplete="current-password"
-                required
-                className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
-                placeholder="Password"
-              />
-            </div>
+        <Form onSubmit={() => {}} className="mt-8 space-y-6">
+          <div>
+            <Label name="email" className="label" errorClassName="label error">
+              Email Address
+            </Label>
+            <TextField
+              name="email"
+              className="input appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+              errorClassName="input error"
+              validation={{
+                required: true,
+                pattern: {
+                  value: /[^@]+@[^\.]+\..+/,
+                },
+              }}
+            />
+            <FieldError name="email" className="error-message" />
           </div>
 
           <div>
-            <button
-              type="submit"
-              className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+            <Label
+              name="password"
+              className="label"
+              errorClassName="label error"
             >
-              <span className="absolute left-0 inset-y-0 flex items-center pl-3">
-                <svg
-                  className="h-5 w-5 text-indigo-500 group-hover:text-indigo-400"
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 20 20"
-                  fill="currentColor"
-                  aria-hidden="true"
-                >
-                  <path
-                    fillRule="evenodd"
-                    d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z"
-                    clipRule="evenodd"
-                  />
-                </svg>
-              </span>
-              Sign in
-            </button>
+              Password
+            </Label>
+            <PasswordField
+              name="password"
+              className="input appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+              errorClassName="input error"
+              validation={{ required: true }}
+            />
+            <FieldError name="message" className="error-message" />
           </div>
-        </form>
+
+          <Submit
+            className="button group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+          >
+            <span className="absolute left-0 inset-y-0 flex items-center pl-3">
+              <svg
+                className="h-5 w-5 text-indigo-500 group-hover:text-indigo-400"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 20 20"
+                fill="currentColor"
+                aria-hidden="true"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z"
+                  clipRule="evenodd"
+                />
+              </svg>
+            </span>
+            Sign In
+          </Submit>
+        </Form>
       </div>
     </div>
   )
