cb362925c59900efcefc0f9e1b3389203b054dad
Oğuzhan Atalay <oguzhnatly@gmail.com>
2021-03-29 13:19:18 +0300

    Custom Input added for StoryBook

--- web/src/components/Input/Input.stories.tsx
+++ web/src/components/Input/Input.stories.tsx
@@ -1,8 +1,46 @@
 import Input from './Input'
 import { Form } from '@redwoodjs/forms'
 
-export const generated = () => {
-  return (<Form><Input name="Email" type="email" /></Form>)
+export const email = () => {
+  return (
+    <Form>
+      <Input
+        name="Email"
+        labelName="Email Address"
+        type="email"
+        style={{ margin: 10 }}
+        inputStyle={{ marginTop: 5 }}
+      />
+    </Form>
+  )
+}
+
+export const password = () => {
+  return (
+    <Form>
+      <Input
+        name="Password"
+        labelName="Password"
+        type="password"
+        style={{ margin: 10 }}
+        inputStyle={{ marginTop: 5 }}
+      />
+    </Form>
+  )
+}
+
+export const anyText = () => {
+  return (
+    <Form>
+      <Input
+        name="Any Text"
+        labelName="Email Address"
+        type="text"
+        style={{ margin: 10 }}
+        inputStyle={{ marginTop: 5 }}
+      />
+    </Form>
+  )
 }
 
 export default { title: 'Components/Input' }
